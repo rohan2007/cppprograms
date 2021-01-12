36e090c490ed5eb2a6466ac54a433cc6ae969563
Rohan Kumar <71839773+rohan2007@users.noreply.github.com>
2021-01-12 20:35:35 +0530

    Create sizeof.cpp

---
+++ sizeof.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Find size of fundamental data types"<<std::endl;
+    std::cout<<"___________________________________"<<std::endl;
+    std::cout<<"The size of char is "<<sizeof(char)<<std::endl;
+    std::cout<<"The size of short is "<<sizeof(short)<<std::endl;
+    std::cout<<"The size of int is "<<sizeof(int)<<std::endl;
+    std::cout<<"The size of long is "<<sizeof(long)<<std::endl;
+    std::cout<<"The size of long long is "<<sizeof(long long)<<std::endl;
+    std::cout<<"The size of float is "<<sizeof(float)<<std::endl;
+    std::cout<<"The size of double is "<<sizeof(double)<<std::endl;
+    std::cout<<"The size of long double is "<<sizeof(long double)<<std::endl;
+    std::cout<<"The size of bool is "<<sizeof(bool)<<std::endl;
+    return 0;
+}
